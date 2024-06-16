0f899a16b7eb3bbf663f22a6b3098c256d7603fe
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-06-16 06:48:01 -0700

    errors

--- errors.go
+++ errors.go
@@ -8,4 +8,5 @@ const (
 	errMissingHexadecimalLiteralPrefix = erorr.Error("ethaddr: missing prefix for hexadecimal-literal (i.e., \"0x\")")
 	errNilDestination                  = erorr.Error("ethaddr: nil destination")
 	errNilReceiver                     = erorr.Error("ethaddr: nil receiver")
+	errNothing                         = erorr.Error("ethaddr: nothing")
 )
